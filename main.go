24f88ccaabdde186dbf9e46ccb597d6e4acda6e2
Shriharsh <shriharshgautam7@gmail.com>
2023-05-06 16:44:25 +0530

    added updateMovie function

--- main.go
+++ main.go
@@ -77,6 +77,27 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	id := params["id"]
+
+	for index, item := range movies {
+		if item.ID == id {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = id
+			movies = append(movies, movie)
+
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+
+	}
+
+}
+
 func main() {
 	r := mux.NewRouter()
 
